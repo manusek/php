0f7bbe12a24af8a9ce484e685a15f245365c7d94
Kacper Długosz <kacper.dlugosz125@gmail.com>
2024-03-21 19:56:33 +0100

    exception in lab2

--- lab2/zad11.php
+++ lab2/zad11.php
@@ -4,10 +4,10 @@ use function PHPSTORM_META\type;
 
 function division($a, $b) : float{
     if($b == 0){
-        throw new Exception("dzielenie przez 0 niedozwolone"); //np sqlexception
+        throw new UnexpectedValueException("dzielenie przez 0 niedozwolone"); //nieoczekiwana wartość
     }
     if (is_int($a) != true || is_int($b) != true){
-        throw new Exception("jedna z liczb nie jest typu INT");
+        throw new InvalidArgumentException("jedna z liczb nie jest typu INT");  //zły argument przekazany do funkcji
     }
     return $a / $b;
 }
